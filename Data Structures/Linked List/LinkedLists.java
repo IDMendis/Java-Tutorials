9a8b0cf57a9d89d5cad7614e345b5654b25298aa
IDMendis <dinushimendisxp@gmail.com>
2025-10-29 23:35:25 +0530

    delete folder file added

---
+++ Data Structures/Linked List/LinkedLists.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.Node;
+
+public class LinkedLists {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        printList(head);
+    }
+}
