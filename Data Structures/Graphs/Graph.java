c791062c64610fd5cf6a13dba6710096b04e4cf3
IDMendis <dinushimendisxp@gmail.com>
2025-10-30 01:02:19 +0530

    graphs

---
+++ Data Structures/Graphs/Graph.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Graph {
+    private Map<Integer, List<Integer>> adjList = new HashMap<>();
+
+    void addEdge(int u, int v) {
+        adjList.putIfAbsent(u, new ArrayList<>());
+        adjList.putIfAbsent(v, new ArrayList<>());
+        adjList.get(u).add(v);
+        adjList.get(v).add(u); // for undirected graph
+    }
+
+    void printGraph() {
+        for (var node : adjList.keySet()) {
+            System.out.println(node + " -> " + adjList.get(node));
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+
+        g.printGraph();
+    }
+}
