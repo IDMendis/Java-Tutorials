5a4ecafffcbfd7e4bf5c3b6f884a565ff19b2fc9
IDMendis <dinushimendisxp@gmail.com>
2025-10-29 23:53:41 +0530

    Level order traversal(breadth first search)

---
+++ Data Structures/Trees/LevelOrderTree.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class LevelOrderTree {
+    static class Node {
+        int data;
+        Node left, right;
+        Node(int data) { this.data = data; }
+    }
+
+    void levelOrder(Node root) {
+        if (root == null) return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            Node temp = q.poll();
+            System.out.print(temp.data + " ");
+            if (temp.left != null) q.add(temp.left);
+            if (temp.right != null) q.add(temp.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        new LevelOrderTree().levelOrder(root);
+    }
+}
