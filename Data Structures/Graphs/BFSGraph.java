8ab6460c20ac0f86b7b355c4891d925b33faa928
IDMendis <dinushimendisxp@gmail.com>
2025-10-30 01:07:50 +0530

    bfs

---
+++ Data Structures/Graphs/BFSGraph.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class BFSGraph {
+    private Map<Integer, List<Integer>> adj = new HashMap<>();
+
+    void addEdge(int u, int v) {
+        adj.putIfAbsent(u, new ArrayList<>());
+        adj.putIfAbsent(v, new ArrayList<>());
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+
+        queue.add(start);
+        visited.add(start);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+            for (int neighbor : adj.get(node)) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BFSGraph g = new BFSGraph();
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 4);
+
+        System.out.print("BFS starting from node 0: ");
+        g.bfs(0);
+    }
+}
