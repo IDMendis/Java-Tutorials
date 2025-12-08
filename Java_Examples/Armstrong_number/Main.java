e2553097cba4d791eda37a1aa5bffc0a37fa474e
IDMendis <dinushimendisxp@gmail.com>
2025-12-08 20:24:12 +0530

    java examples

---
+++ Java_Examples/Armstrong_number/Main.java
@@ -0,0 +1,18 @@
+package Java_Examples.Armstrong_number;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 153;
+        int original = num;
+        int result = 0;
+        int digits = String.valueOf(num).length();
+
+        while (num != 0) {
+            int digit = num % 10;
+            result += Math.pow(digit, digits);
+            num /= 10;
+        }
+
+        System.out.println(original + (result == original ? " is Armstrong" : " is not Armstrong"));
+    }
+}
